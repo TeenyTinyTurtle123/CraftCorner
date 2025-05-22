a0202bc088c39df9f246dad59c27f6a48fc22a63
Sofia Gustavsson <Sofia.Gustavsson@rudholmgroup.com>
2025-05-22 14:19:40 +0200

    Style Profile page a bit

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,20 +1,16 @@
+import { ColorBadge } from "@/components/ColorBadge";
 import { ProfileWIPCard } from "@/components/ProfileWIPCard";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { useUser } from "@/Context";
-import { Project } from "@/types/project";
+import { Project, ProjectType } from "@/types/project";
+import { BadgePlus } from "lucide-react";
 import { useEffect, useState } from "react";
 import DefaultProfile from "../assets/DefaultProfile.png";
 
 export function Profile() {
   const [project, setProject] = useState<Project[]>([]);
   const { user } = useUser();
-
-  // useEffect(() => {
-  //   // Get all projects in the database
-  //   fetch("https://localhost:44373/Project/GetAll")
-  //     .then((res) => res.json())
-  //     .then((json: Project[]) => setProject(json));
-  // }, []);
 
   useEffect(() => {
     if (!user?.id) return;
@@ -34,51 +30,93 @@ export function Profile() {
 
   const wipCount = project.filter((p) => p.status === "WIP").length;
   const finishedCount = project.filter((p) => p.status === "Finished").length;
+  const wipProjects = project.filter((p) => p.status === "WIP");
+
+  function getMostUsedProjectType(projects: { projectType: ProjectType }[]) {
+    const typeCount: Record<ProjectType, number> = {
+      Other: 0,
+      Crochet: 0,
+      Knit: 0,
+      CrossStitch: 0,
+      Embroidery: 0,
+    };
+
+    projects.forEach((p) => {
+      typeCount[p.projectType]++;
+    });
+
+    let mostUsed: ProjectType = "Other";
+    let maxCount = 0;
+
+    for (const type in typeCount) {
+      if (typeCount[type as ProjectType] > maxCount) {
+        maxCount = typeCount[type as ProjectType];
+        mostUsed = type as ProjectType;
+      }
+    }
+
+    return mostUsed;
+  }
+
+  const mostUsedType = getMostUsedProjectType(project);
+
+  //TODO: sm:flex-row this seems to be how to make it to mobile!! the sm: look into it
 
   return (
     <>
-      <h1>Profile page</h1>
       {/* profile picture and information */}
       <div className="max-w-screen-lg mx-auto p-4">
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            padding: "1rem",
-            border: "1px red solid",
-          }}
-        >
+        <div className="flex justify-between items-center bg-teal-50 rounded-md mb-3 p-4">
           <img
-            style={{
-              width: "8rem",
-              height: "8rem",
-              borderRadius: "50%",
-              objectFit: "cover",
-              margin: "1rem",
-              border: "1px blue solid",
-            }}
+            className="w-50 h-50 rounded-full border-2 object-cover"
             src={DefaultProfile}
           />
-          <div>
-            <p>
-              <strong>Name: {user?.username}</strong>
+
+          <div className="flex-1 ml-4 self-start">
+            <p className="border-b text-left font-semibold text-xl max-w-80">
+              {user?.username}
             </p>
-            <p>
-              <strong>Country:</strong> Norway
+            <p className="text-left pt-2">
+              Most used tag: <ColorBadge children={mostUsedType} />
             </p>
           </div>
+
+          <div className="mr-10">
+            <Button className="bg-teal-600 text-lg font-semibold hover:bg-teal-400">
+              Create new <BadgePlus />
+            </Button>
+          </div>
         </div>
-        <div>
-          <Button>WIP ({wipCount})</Button>
-          <Button>Finished ({finishedCount})</Button>
+
+        {/* Display WIP and Finished projects */}
+        <div className="mb-3 border-b text-left pb-4">
+          <p className="font-semibold pb-2">
+            {user?.username.toLocaleUpperCase()}'S LIBRARY
+          </p>
+          <Badge className="bg-indigo-400 text-white text-base rounded-full mr-2">
+            WIP ({wipCount})
+          </Badge>
+          <Badge className="bg-teal-600 text-white text-base rounded-full mr-2">
+            FINISHED ({finishedCount})
+          </Badge>
           {/* Add this to user table */}
-          <Button>FLoss (56)</Button>
+          <Badge className="bg-sky-600 text-white text-base rounded-full">
+            FLOSS (56)
+          </Badge>
         </div>
+
         {/* Display inprogress work */}
-        <div className="grid grid-cols-2 gap-2">
-          {project.map((p) => (
-            <ProfileWIPCard key={p.id} project={p} />
-          ))}
+        <div>
+          <div className="mb-3 border-b text-left">
+            <p className="font-semibold pb-2">
+              {user?.username.toLocaleUpperCase()} IS CURRENTLY WORKING ON
+            </p>
+          </div>
+          <div className="grid grid-cols-2 gap-2">
+            {wipProjects.map((p) => (
+              <ProfileWIPCard key={p.id} project={p} />
+            ))}
+          </div>
         </div>
       </div>
     </>
