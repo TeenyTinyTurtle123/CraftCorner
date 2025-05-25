5dd006107335afcee41abe1ad47fe57d34565d98
Sofia Gustavsson <Sofia.Gustavsson@rudholmgroup.com>
2025-05-25 14:46:52 +0200

    Mobile size header(almost)

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,38 +1,35 @@
 import { useUser } from "@/Context";
 import { Label } from "@radix-ui/react-label";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/Logotransparant.png";
 import { Dropdown } from "./Dropdown";
 
 export function Header() {
   const { user } = useUser();
+  const navigate = useNavigate();
 
   return (
     <header className="fixed top-0 left-0 right-0 h-24 bg-white border-b-2 border-t-2 border-teal-300 p-4 z-50 flex items-center justify-between ">
       <div className="z-10">
-        <img className="h-12 w-auto" src={logo} alt="Logo" />
+        <img
+          className="h-12 w-auto cursor-pointer"
+          src={logo}
+          alt="Logo"
+          onClick={() => navigate(`/`)}
+        />
       </div>
-      <nav className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-6">
-        <Link className="hover:text-teal-300 font-semibold text-lg" to="/">
-          Home
-        </Link>
-        <Link
-          className="hover:text-teal-300 font-semibold text-lg"
-          to={`/library/${user?.id}`}
-        >
+      <nav className="hidden absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 md:flex gap-6">
+        <Link className="hover:text-teal-300 font-semibold text-lg">
           Library
         </Link>
-        <Link
-          className="hover:text-teal-300 font-semibold text-lg"
-          to="/addProject"
-        >
-          Add Project
+        <Link className="hover:text-teal-300 font-semibold text-lg">
+          Stitch Count
         </Link>
         <Link
           className="hover:text-teal-300 font-semibold text-lg"
           to={`/profile/${user?.id}`}
         >
-          Profile
+          CrossStitch
         </Link>
         <Link
           className="hover:text-teal-300 font-semibold text-lg"
@@ -46,7 +43,9 @@ export function Header() {
       <div className="z-10 flex items-center gap-2">
         {user ? (
           <>
-            <Label className="text-lg">Welcome {user.username}!</Label>
+            <Label className="text-lg hidden sm:block">
+              Welcome {user.username}!
+            </Label>
             <Dropdown />
           </>
         ) : (
